693e5ddd71a1408ea3eda5a5fbdaf5431ae50e82
Myllena Cruz <myllena.ocruz@gmail.com>
2023-01-16 17:18:33 -0300

    refactoring turn left and right adding execute function

--- src/tests/Movements.spec.ts
+++ src/tests/Movements.spec.ts
@@ -1,7 +1,8 @@
 import { Coordinates } from "../movements/types/Coordinates";
 import { Directions } from "../movements/types/Directions";
+import { VehicleMovements } from "../movements/types/VehicleMovements";
 
-function turnLeft(direction: Directions): string | undefined {
+function turnLeft(direction: Directions): Directions {
     switch(direction) {
         case "N":
             return "O";
@@ -17,7 +18,7 @@ function turnLeft(direction: Directions): string | undefined {
     }
 }
 
-function turnRight(direction: Directions): string | undefined {
+function turnRight(direction: Directions): Directions {
     switch(direction) {
         case "N":
             return "L";
@@ -45,27 +46,60 @@ function move(
     if (direction === "O") return [x - 1, y];
 }
 
+function execute(
+    command: string, 
+    initialState: VehicleMovements
+): VehicleMovements | undefined {
+    if (command === "E")
+        return {
+            ...initialState,
+            direction: turnLeft(initialState.direction)
+        }
+
+    if (command === "D")
+        return {
+            ...initialState,
+            direction: turnRight(initialState.direction)
+        }
+}
+
 describe("Possible Movements", () => {
     test.each`
-        direction | expected
+        forward   | expected
         ${"N"}    | ${"O"}
         ${"O"}    | ${"S"}
         ${"S"}    | ${"L"} 
         ${"L"}    | ${"N"} 
         `
-        ("When facing $direction, turning left should cause to face $expected", ({ direction, expected }) => {  
-            expect(turnLeft(direction)).toBe(expected); 
+        ("When facing $forward, turning left should cause to face $expected", ({ forward, expected }) => {  
+            const initialState: VehicleMovements = {
+                direction: forward,
+                position: [1, 1]
+            };
+
+            expect(execute("E", initialState)).toEqual({
+                ...initialState,
+                direction: expected
+            });
         });
     
     test.each`
-        direction | expected
+        forward   | expected
         ${"N"}    | ${"L"}
         ${"S"}    | ${"O"}
         ${"L"}    | ${"S"} 
         ${"O"}    | ${"N"} 
         `
-        ("When facing $direction, turning right should cause to face $expected", ({ direction, expected }) => {
-            expect(turnRight(direction)).toBe(expected);
+        ("When facing $forward, turning right should cause to face $expected", ({ forward, expected }) => {
+            const initialState: VehicleMovements = {
+                direction: forward,
+                position: [1, 1]
+            };
+
+            expect(execute("D", initialState)).toEqual({
+                ...initialState,
+                direction: expected
+            });
         });
 
     test("When moving N should increment the Y coordinate", () => {
