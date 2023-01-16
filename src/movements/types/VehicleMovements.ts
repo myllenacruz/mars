b9ab7c74b46d39404ed592f1215c1cc9d8605172
Myllena Cruz <myllena.ocruz@gmail.com>
2023-01-16 17:13:01 -0300

    add type vehicleMovements

---
+++ src/movements/types/VehicleMovements.ts
@@ -0,0 +1,7 @@
+import { Directions } from "./Directions";
+import { Coordinates } from "./Coordinates";
+
+export type VehicleMovements = {
+    direction: Directions,
+    position: Coordinates
+}
