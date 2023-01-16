7da1fa83ff9e8ed0395f01fb0c6341b332aff77a
Myllena Cruz <myllena.ocruz@gmail.com>
2023-01-16 19:44:55 -0300

    add turn class

---
+++ src/movements/Turn.ts
@@ -0,0 +1,35 @@
+import { Directions } from "./types/Directions";
+
+export class Turn {
+    public left(direction: Directions): Directions {
+        switch(direction) {
+            case "N":
+                return "O";
+    
+            case "O":
+                return "S";
+    
+            case "S":
+                return "L";
+    
+            case "L":
+                return "N";
+        };
+    };
+
+    public right(direction: Directions): Directions {
+        switch(direction) {
+            case "N":
+                return "L";
+    
+            case "L":
+                return "S";
+    
+            case "S":
+                return "O";
+    
+            case "O":
+                return "N";
+        };
+    };
+}
