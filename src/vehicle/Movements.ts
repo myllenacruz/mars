8d4a8d4e806fbae1e05caf6a03d5bcef35a5924f
Myllena Cruz <myllena.ocruz@gmail.com>
2023-01-17 20:43:52 -0300

    refactoring movements class

--- src/vehicle/Movements.ts
+++ src/vehicle/Movements.ts
@@ -2,37 +2,29 @@
 import { VehicleMovements } from "../vehicle/types/VehicleMovements";
 import { Turn } from "./Turn";
 import { Move } from "./Move";
+import { Directions } from "./types/Directions";
 
 export class Movements {
-    private applyCommand(
-        state: VehicleMovements,
-        command: string
-    ): VehicleMovements {
-        const turn = new Turn();
-        const move = new Move();
-
-        if (command === "E")
-            return {
-                ...state,
-                direction: turn.left(state.direction)
-            };
-    
-        if (command === "D")
-            return {
-                ...state,
-                direction: turn.right(state.direction)
-            };
-    
-        if (command === "M") return move.execute(state)
-
-        return state;
-    }
-
     public execute(
         command: string, 
         state: VehicleMovements
     ): VehicleMovements {
         const movements = command.split("").reduce(this.applyCommand, state);
         return movements;
+    }
+    
+    private applyCommand(
+        state: VehicleMovements,
+        command: string
+    ): VehicleMovements {
+        const turn = new Turn();
+        const move = new Move();
+        let direction: Directions = "" as Directions;
+
+        if (command === "E") direction = turn.left(state.direction);
+        if (command === "D") direction = turn.right(state.direction);
+        if (command === "M") return move.execute(state);
+
+        return { ...state, direction };
     }
 }
