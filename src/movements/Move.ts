132e5dfb2746c60efc7d9b4b23e3a1ffe1368801
Myllena Cruz <myllena.ocruz@gmail.com>
2023-01-16 22:15:52 -0300

    improvements on move class

--- src/movements/Move.ts
+++ src/movements/Move.ts
@@ -1,18 +1,17 @@
 import { Coordinates } from "./types/Coordinates";
-import { Directions } from "./types/Directions";
+import { VehicleMovements } from "./types/VehicleMovements";
 
 export class Move {
     public execute(
-        direction: Directions, 
-        position: Coordinates
+        vehicleMovements: VehicleMovements
     ): Coordinates {
-        const [x, y] = position;
+        const [x, y] = vehicleMovements.position;
     
-        if (direction === "N") return [x, y + 1];
-        if (direction === "L") return [x + 1, y];
-        if (direction === "S") return [x, y - 1];
-        if (direction === "O") return [x - 1, y];
+        if (vehicleMovements.direction === "N") return [x, y + 1];
+        if (vehicleMovements.direction === "L") return [x + 1, y];
+        if (vehicleMovements.direction === "S") return [x, y - 1];
+        if (vehicleMovements.direction === "O") return [x - 1, y];
     
-        return position;
+        return vehicleMovements.position;
     };
 }
