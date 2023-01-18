80795156798a847fad392094c0826c8209a21f57
Myllena Cruz <myllena.ocruz@gmail.com>
2023-01-18 14:25:18 -0300

    add moduleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,15 @@
+import { pathsToModuleNameMapper } from "ts-jest";
+
 export default {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  moduleNameMapper: pathsToModuleNameMapper(
+		{
+			"@vehicle/*": ["vehicle/*"]
+		},
+		{ prefix: "<rootDir>/src/" }
+	),
 	collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
 	coveragePathIgnorePatterns: ["/node_modules/"],
   preset: "ts-jest",
