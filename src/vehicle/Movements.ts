6674a9b98199b9ec0e8a71c5ccbc8c07c7403ca8
Myllena Cruz <myllena.ocruz@gmail.com>
2023-01-18 18:27:32 -0300

    improvements on getPosition function

--- src/vehicle/Movements.ts
+++ src/vehicle/Movements.ts
@@ -72,12 +72,19 @@ export class Movements {
         position: Coordinates
     ): Coordinates {
         const [x, y] = position;
-    
-        if (direction === "N") return [x, y + 1];
-        if (direction === "L") return [x + 1, y];
-        if (direction === "S") return [x, y - 1];
-        if (direction === "O") return [x - 1, y];
-    
-        return position;
+
+        switch(direction) {
+            case "N":
+                return [x, y + 1]
+            
+            case "L":
+                return [x + 1, y]
+            
+            case "S":
+                return [x, y - 1]
+            
+            case "O":
+                return [x - 1, y]
+        }
     };
 }
